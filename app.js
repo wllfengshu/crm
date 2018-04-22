897eaf95d25865024c06fa8ab5759b103e52470c
wllfengshu <wllfengshu@163.com>
2018-04-22 23:36:43 +0800

    完成登录

--- app.js
+++ app.js
@@ -10,22 +10,6 @@ var views = require('./routes/views');
 
 var app = express();
 
-//var allowCrossDomain = function (req, res, next) {
-//	res.header('Access-Control-Allow-Origin', '*');
-//res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-//res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-//
-//if (req.method == 'OPTIONS') {
-//  res.send(200); /让options请求快速返回/
-//}
-//else {
-//  next();
-//}
-//};
-//app.use(allowCrossDomain);
-
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 //注册ejs模板为html页。简单的讲，就是原来以.ejs为后缀的模板页，现在的后缀名可以是.html了
@@ -37,7 +21,9 @@ app.set('view engine', 'html');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -46,20 +32,20 @@ app.use('/views', views);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
